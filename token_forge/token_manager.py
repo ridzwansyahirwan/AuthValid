228dfe4113bf0d848b33428db9287e28a251a469
KACS - Mohammad Ridzwan Syah Irwan <ridzwansyahirwan@gmail.com>
2024-11-21 11:59:43 +0800

    update token manager

--- token_forge/token_manager.py
+++ token_forge/token_manager.py
@@ -1,11 +1,9 @@
-from flask import Flask
 import os
 import secrets
 import jwt
 import logging
 from dotenv import load_dotenv, set_key
-from pathlib import Path
-from datetime import datetime, timedelta
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -13,7 +11,7 @@ logging.basicConfig(filename='app.log',
                     level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-env_path = Path(__file__).resolve().parent.parent / '.env'
+# Load environment variables from .env file
 load_dotenv()
 
 def generate_secret_key():
@@ -26,18 +24,21 @@ def set_secret_key():
         set_key('.env', 'SECRET_KEY', secret_key)
     return secret_key
 
-def generate_token(user_id, username, roles, expiration_minutes=60):
+def generate_token(user_id, username, roles):
     payload = {
         'user_id': user_id,
         'username': username,
-        'roles': roles,
-        'exp': datetime.utcnow() + timedelta(minutes=expiration_minutes)
+        'roles': roles
+        # No expiration time
     }
     secret_key = set_secret_key()
     token = jwt.encode(payload, secret_key, algorithm='HS256')
     set_key('.env', 'AUTHORIZED_TOKEN', token)
     logging.info(f"Token generated for user: {username}")
     return token
+
+def get_authorized_token():
+    return os.getenv('AUTHORIZED_TOKEN')
 
 def refresh_token(token):
     try:
@@ -48,20 +49,23 @@ def refresh_token(token):
             username=decoded_token['username'],
             roles=decoded_token['roles']
         )
+        set_key('.env', 'AUTHORIZED_TOKEN', new_token)
         return new_token
     except Exception as e:
         logging.error(f"Error refreshing token: {e}")
         return None
 
 def authenticate_token(token):
+    authorized_token = get_authorized_token()
+    if token != authorized_token:
+        logging.error("Token does not match the authorized token.")
+        return False, None
+
     try:
         secret_key = set_secret_key()
         decoded_token = jwt.decode(token, secret_key, algorithms=['HS256'])
         logging.info("Token authenticated successfully")
         return True, decoded_token
-    except jwt.ExpiredSignatureError:
-        logging.error("Token has expired.")
-        return False, None
     except jwt.InvalidTokenError:
         logging.error("Invalid token.")
         return False, None
